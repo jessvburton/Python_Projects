8bba8c36b884212cbbde7ce09ce8eb7fbf6271e2
Jessica Burton <jessvburton@gmail.com>
2025-07-23 17:59:08 +0100

    part 3

--- intermediate/us_states_game/main.py
+++ intermediate/us_states_game/main.py
@@ -1,5 +1,6 @@
 import turtle
 import pandas as pd
+from numpy.ma.core import append
 
 image = "blank_states_img.gif"
 states_file = "50_states.csv"
@@ -23,12 +24,18 @@ answer_state = screen.textinput("Guess the State", prompt="What is a State's nam
 while len(states_guessed) < 50:
     screen.update()
 
+    if answer_state == 'Exit':
+        missing_states = []
+        for state in states_list:
+            if state not in states_guessed:
+                missing_states.append(state)
+            new_data = pd.DataFrame(missing_states)
+            new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in states_list:
         answer = data[data.state == answer_state]
         state.goto(answer.x.item(), answer.y.item())
         state.write(answer_state) # write correct guess on map
-        states_guessed.append(answer)  # record correct guesses in a list
+        states_guessed.append(answer_state)  # record correct guesses in a list
 
     answer_state = screen.textinput(title=f"{len(states_guessed)}/50 States Correct", prompt="What is a State's name?").title() # keep track of score
-
-screen.exitonclick()
