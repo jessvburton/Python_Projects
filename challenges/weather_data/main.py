b398091b1131fd358fcf6afc677a429de22a94e9
Jessica Burton <jessvburton@gmail.com>
2025-07-23 15:21:00 +0100

    Squirrel census

--- challenges/weather_data/main.py
+++ challenges/weather_data/main.py
@@ -17,4 +17,22 @@
 import pandas as pd
 
 data = pd.read_csv("weather_data.csv")
-print(data["temp"])
+
+temp_list = data["temp"].to_list()
+avg_temp = sum(temp_list) / len(temp_list)
+print(avg_temp)
+
+print(data["temp"].mean())
+
+print(data["temp"].max())
+
+print(data[data.day == "Monday"])
+
+print(data[data["temp"] == data["temp"].max()])
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+# (0°C × 9/5) + 32
+f = (monday.temp * 9/5) + 32
+print(f)
